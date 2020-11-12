0e2330345ea565bfc548d28b2ef6dcf04371699e
Michael BAUDIN <michael.baudin@gmail.com>
2020-11-12 21:23:29 +0100

    Améliore les scripts

---
+++ tests/cleanup.sh
@@ -0,0 +1,8 @@
+git rm -r ../1-Intro-OT/Solutions
+git rm -r ../2-Quantification/Solutions
+git rm -r ../3-Propagation/Solutions
+git rm -r ../4-Sensibilite/Solutions
+git rm -r ../5-Chaos/Solutions
+git rm -r ../6-krigeage/Solutions
+git rm -r ../8-Calage/Solutions
+
