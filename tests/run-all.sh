6dacc8b62df0433c7d1869e791f65b2aa463f1da
Michael BAUDIN <michael.baudin@gmail.com>
2024-12-30 19:44:10 +0100

    Update to OT124

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -10,7 +10,16 @@ python -c "import openturns; print(openturns.__version__); exit()"
 # Run tests
 cd ..
 
-# Notebooks in all subdirectories
-python tests/find-ipynb-files.py
+# Execute IPython Notebooks in all subdirectories
+python tests/find-ipynb-files.py .
 
-
+# Execute Python scripts in all subdirectories
+python tests/find-py-files.py 0-Deroulement
+python tests/find-py-files.py 1-Intro-OT
+python tests/find-py-files.py 2-Quantification
+python tests/find-py-files.py 3-Propagation
+python tests/find-py-files.py 4-Sensibilite
+python tests/find-py-files.py 5-Chaos
+python tests/find-py-files.py 6-krigeage
+python tests/find-py-files.py 8-Calage
+python tests/find-py-files.py 9-Fiabilite
